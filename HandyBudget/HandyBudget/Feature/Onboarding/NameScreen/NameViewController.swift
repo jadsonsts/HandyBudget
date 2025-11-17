f6d95bea6f7f14feb4ae480a033216027eb034c3
jadsonsts <jadson.s.carneiro@gmail.com>
2025-11-17 22:55:27 +1300

    initial commit with color set

---
+++ HandyBudget/HandyBudget/Feature/Onboarding/NameScreen/NameViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HandyBudget
+//
+//  Created by Jadson on 13/11/2025.
+//
+
+import UIKit
+
+class NameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
